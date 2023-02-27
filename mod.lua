a7434a81497c9f79530367bd27d7f0d02abdccbb
Florian Thiel <flth@flth.de>
2023-02-27 16:18:23 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,24 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "truck", "man", },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,49 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "MAN \"chubby cheek\"",
+		["mod_desc"] = "This mod adds MAN trucks known as \"chubby cheek\". They had different model numbers. The design of the new model family was similar to the heavier hooded wagons. To emphasize the family resemblance, it received slight bulges at the front corners - where the fenders ended on the hood - which earned the nickname of the vehicles equipped in this way.",
+
+		["man_pb_1_desc"] = "The design of the new model family was similar to the heavier hooded wagons. To emphasize the family resemblance, it was given slight bulges at the front corners - where the fenders ended on the hood - which earned the vehicles equipped in this way the nickname “chubby”. The model achieved great popularity, especially in long-haul transport with a long cab with the types 10.212 F/FS (and other names). The chubby cheek was built until 1967.",
+        ["man_pb_menu"] = "MAN \"chubby cheek\"",
+		["man_pb_1_name"] = "Food transport",
+		["man_pb_hanger_1_name"] = "Food transport w/ trailer",
+		["man_pb_sattel_1_name"] = "Semi-trailer Food",
+		["man_pb_2_name"] = "Dump truck",
+		["man_pb_hanger_2_name"] = "Dump truck w/ trailer",
+		["man_pb_sattel_2_name"] = "Semi-trailer Dump truck",
+		["man_pb_3_name"] = "Flat bed",
+		["man_pb_hanger_3_name"] = "Flat bed w/ trailer",
+		["man_pb_sattel_3_name"] = "Semi-trailer Flat bed",
+		["man_pb_4_name"] = "Flat bed with Canopy",
+		["man_pb_hanger_4_name"] = "Flat bed with Canopy w/ trailer",
+		["man_pb_sattel_4_name"] = "Semi-trailer Flat bed with Canopy",
+        ["man_pb_sattel_5_name"] = "Semi-trailer Tank",
+
+		},
+
+	de = {
+
+		["mod_name"] = "MAN \"Pausbacke\"",
+		["mod_desc"] = "Dieser Mod fügt MAN Lastwagen hinzu, die unter dem Namen \"Pausbacke\" bekannt wurden. Sie hatten verschiedene Modellnummern. Konstruktiv ähnelte die neue Modellfamilie den schwereren Haubenwagen. Zur Betonung der Familienähnlichkeit erhielt sie an den vorderen Ecken – dort wo beim Hauber die Kotflügel endeten – leichte Ausbuchtungen, die den so ausgestatteten Fahrzeugen den Spitznamen einbrachten.",
+
+		["man_pb_1_desc"] = "Konstruktiv ähnelte die neue Modellfamilie den schwereren Haubenwagen. Zur Betonung der Familienähnlichkeit erhielt sie an den vorderen Ecken – dort wo beim Hauber die Kotflügel endeten – leichte Ausbuchtungen, die den so ausgestatteten Fahrzeugen den Spitznamen \"Pausbacke\" einbrachten. Das Modell erlangte vor allem im Fernverkehr mit langem Fahrerhaus bei den Typen 10.212 F/FS (und weiteren Bezeichnungen) große Beliebtheit. Gebaut wurde die Pausbacke bis 1967.",
+        ["man_pb_menu"] = "MAN \"Pausbacke\"",
+		["man_pb_1_name"] = "Kühltransporter",
+		["man_pb_hanger_1_name"] = "Kühltransporter mit Anhänger",
+		["man_pb_sattel_1_name"] = "Sattelauflieger Kasten",
+		["man_pb_2_name"] = "Kipper",
+		["man_pb_hanger_2_name"] = "Kipper mit Anhänger",
+		["man_pb_sattel_2_name"] = "Sattelauflieger Kipper",
+		["man_pb_3_name"] = "Pritsche offen",
+		["man_pb_hanger_3_name"] = "Pritsche offen mit Anhänger",
+		["man_pb_sattel_3_name"] = "Sattelauflieger Pritsche offen",
+		["man_pb_4_name"] = "Pritsche mit Plane",
+		["man_pb_hanger_4_name"] = "Pritsche mit Plane und Anhänger",
+		["man_pb_sattel_4_name"] = "Sattelauflieger Pritsche mit Plane",
+        ["man_pb_sattel_5_name"] = "Sattelauflieger Tanker",
+
+		},
+}
+end
